4cbee4591a668d160285ccce36b438b3151dd9f7
Vetka20 <961497869@ukr.net>
2023-09-14 13:48:31 +0300

    database

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h4>Aircraft</h4>')
+    return render(request, 'main/index.html')
 
 def about(request):
     return HttpResponse('<h1>About</h1>')
